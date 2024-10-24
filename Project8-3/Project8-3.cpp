ecc23e3e1155453aa7e14939db3396547b65c0d5
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-24 09:27:23 -0400

    Completed P8-3.

--- Project8-3/Project8-3.cpp
+++ Project8-3/Project8-3.cpp
@@ -5,9 +5,19 @@
 using namespace std;
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
 
 int main(){
-	
+	string input;
+
+	while (true) {
+		cout << "Enter Roman number or Q to quit: ";
+		getline(cin, input);
+		if (input == "Q") break;
+		cout << convertRomanToInt(input) << endl;
+	}
+
+	return 0;
 
 }
 
@@ -23,4 +33,21 @@ int romanCharValue(char r) {
 	case 'M': return 1000;
 	}
 	
+}
+
+int convertRomanToInt(string s) {
+
+	int total = 0;
+
+		for (int i = 0; i < s.length(); i++) {
+			if ((romanCharValue(s[i]) >= romanCharValue(s[i + 1])) || s.length() == 1) {
+				total += romanCharValue(s[i]);
+			}
+			else {
+				total += romanCharValue(s[i + 1]) - romanCharValue(s[i]);
+				i++;
+			}
+		}
+
+		return total;
 }
