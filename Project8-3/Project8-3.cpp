132fc10658fad3751be2142e100b7b8c59b21081
CMacSwam <c-mccluskey.1@onu.edu>
2024-10-24 09:12:38 -0400

    Completed romanCharValue function.

---
+++ Project8-3/Project8-3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include<cstring>
+
+using namespace std;
+
+int romanCharValue(char r);
+
+int main(){
+	
+
+}
+
+int romanCharValue(char r) {
+
+	switch (r) {
+	case 'I': return 1;
+	case 'V': return 5;
+	case 'X': return 10;
+	case 'L': return 50;
+	case 'C': return 100;
+	case 'D': return 500;
+	case 'M': return 1000;
+	}
+	
+}
